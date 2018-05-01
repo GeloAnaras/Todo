a12361d3bda01ae0a380a6a53e010e7a8bec668c
GeloAnaras <doxifera@gmail.com>
2018-05-01 13:06:35 +0300

    create view

---
+++ view.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        <form action="add" method="post">
+            <input type="text" name="text" placeholder="type text here">
+            <input type="submit">
+        </form>
+        <ul>
+            <li>Note one
+                <a href="del"></a>
+            </li>
+            <li>Note two
+                <a href="del"></a>
+            </li>
+            <li>Note three
+                <a href="del"></a>
+            </li>
+        </ul>
+    </div>
+</body>
+</html>
+
+
+
+<!--вставки вывода сделаю после-->
